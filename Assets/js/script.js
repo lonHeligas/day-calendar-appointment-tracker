c75c4359ff5dbbccc86192c23dcfbd948f370ccd
Lon Heligas <lon.heligas@gmail.com>
2023-01-11 21:48:00 -0600

    cleanup of comments

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,6 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// let incomingSched = []; ???
 
 
-// array of objects: hour
-//                   appt
+// this is a dummy block of content that was/or will be overwritten by the local storage
 
 var schedule = [
   {
@@ -42,8 +37,7 @@ $(function () {
     // console.log(currentTimeDay);
     var currentDayEl = $('#currentDay');
     currentDayEl.text(currentTimeDay.format('MMMM Do, YYYY'));
-    // this displays the date at the top of the day when the page is loaded
-    // getSchedule();
+    // this displays the date at the top of the day when the page is loaded    
     renderSchedule()
   }
 
@@ -82,17 +76,15 @@ $(function () {
       schedule = [];
     };
     console.log(schedule);
-    // render    
+    
     renderIncomingData();
     
-    // let downloadSchedule = something;
-    // this will grab the current schedule from the local storage
+    
   }
   
   
   function renderIncomingData (){  
-    // console.log("you are in renderIncomingData");
-    
+    // console.log("you are in renderIncomingData");    
     for (i = 0; i <schedule.length; i++){
       var currHourObj = schedule[i];
       // console.log(currHourObj.hour);
@@ -106,15 +98,12 @@ $(function () {
       $(`${idWeWant} .description`).text(currHourAppt);      
       // console.warn("out of the if loop")   ;
       
-      
     };
     
   saveButtonListen();
   };
   
-  // i have to click a button
-  // it saves the content in the text field corresponding to that button into the array
-  // it saves the array in local storage
+  
 
   function saveButtonListen(){
     
@@ -145,7 +134,7 @@ $(function () {
 
 
       
-      //saveSchedule(whichButton);
+      
     });   
 
   }
@@ -153,32 +142,10 @@ $(function () {
 
   
   
-  function saveSchedule (thisButton){    
-
-    
-    
-    // this will save the current schedule in local storage when the user clicks save
-  }
+ 
   
   
   
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
   
   
   
@@ -189,9 +156,7 @@ $(function () {
     
     
     
-    //saveSchedule();
     
-    // this is the starter function that fires up when the user loads the page
   }
 
   start ();
